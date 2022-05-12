29c74cf7ecc1f1f27eef340aaaa7e82f1b2f61ba
Krb0 <maurodavidcaceres@gmail.com>
2022-05-12 14:17:39 -0300

    text added

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,18 @@ const Home: NextPage = () => {
         <title>FakeYa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="md:bg-food h-[85vh] ">
-        <h1>FakeYa</h1>
+      <main className="md:bg-food flex h-[85vh] flex-col items-center justify-center gap-10 pb-[60px] text-white ">
+        <div className="font-lato ">
+          <h1 className="text-[2.5rem] font-[700]">¡Pedí lo que quieras!</h1>
+          <h2 className="text-[1rem]">
+            Restaurantes, mercados, farmacias, kioscos y mucho más.
+          </h2>
+        </div>
+        <div className="h-[90px] w-[60%] ">
+          <span className="font-muli text-[1.05rem] font-[800] ">
+            Ingresá tu dirección
+          </span>
+        </div>
       </main>
     </Layout>
   )
