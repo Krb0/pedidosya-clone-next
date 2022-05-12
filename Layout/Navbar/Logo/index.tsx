911f0fd2d222a9ff40605bb34b3568f6ebda115c
Krb0 <maurodavidcaceres@gmail.com>
2022-05-11 23:45:30 -0300

    priority added

--- Layout/Navbar/Logo/index.tsx
+++ Layout/Navbar/Logo/index.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <a className="relative h-[40px] w-[170px] ">
-        <Image src={LogoImg} layout="fill" className="absolute" />
+        <Image src={LogoImg} layout="fill" className="absolute" priority />
       </a>
     </Link>
   )
