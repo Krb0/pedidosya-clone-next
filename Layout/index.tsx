8a47a53fb1369dd615732d42acbec200edb0c3f4
Krb0 <maurodavidcaceres@gmail.com>
2022-05-12 14:17:24 -0300

    font added

--- Layout/index.tsx
+++ Layout/index.tsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full">
+    <div className="font-lato w-full">
       <Navbar />
       {children}
     </div>
