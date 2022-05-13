8bf2cc7ecdc762de8bbb313f420351083669fa0f
Krb0 <maurodavidcaceres@gmail.com>
2022-05-13 00:46:07 -0300

    input added

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,15 +12,23 @@ const Home: NextPage = () => {
       </Head>
       <main className="md:bg-food flex h-[85vh] flex-col items-center justify-center gap-10 pb-[60px] text-white ">
         <div className="font-lato ">
-          <h1 className="text-[2.5rem] font-[700]">¡Pedí lo que quieras!</h1>
+          <h1 className="text-[2.75rem] font-[700]">¡Pedí lo que quieras!</h1>
           <h2 className="text-[1rem]">
             Restaurantes, mercados, farmacias, kioscos y mucho más.
           </h2>
         </div>
-        <div className="h-[90px] w-[60%] ">
-          <span className="font-muli text-[1.05rem] font-[800] ">
+        <div className="flex w-[60%] flex-col gap-4">
+          <span className="font-muli text-[1.05rem] font-[800]">
             Ingresá tu dirección
           </span>
+          <div className="flex h-[64px] w-[100%] ">
+            <div className="flex h-full w-[100%] items-center justify-center rounded-full bg-white">
+              <input className="h-full" placeholder="Ingresa" />
+            </div>
+            <button className="ml-[20px] h-full w-[160px] rounded-full bg-[#FA0050]">
+              Buscar
+            </button>
+          </div>
         </div>
       </main>
     </Layout>
