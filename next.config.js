2ffe19347d5be7e23a5baae29c49311eda987ce8
Krb0 <maurodavidcaceres@gmail.com>
2022-05-16 18:17:48 -0300

    img domain added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['play-lh.googleusercontent.com'],
+    domains: ['play-lh.googleusercontent.com', 'img.pystatic.com'],
   },
 }
