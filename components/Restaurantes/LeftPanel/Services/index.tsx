f7d7e10bf79e1db81ab396b0365ad23408ea8e54
Krb0 <maurodavidcaceres@gmail.com>
2022-05-18 20:08:44 -0300

    active state added

--- components/Restaurantes/LeftPanel/Services/index.tsx
+++ components/Restaurantes/LeftPanel/Services/index.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
 import services from './data.json'
 const Services = () => {
+  const [active, setActive] = useState(1)
   return (
-    <div className="space-between flex flex-wrap">
+    <div className="space-between flex flex-wrap ">
       {services.map(({ name, img, id }) => (
         <div
-          className="mt-[15px] flex basis-[33%] flex-col items-center "
+          className="max-width-[34%] mt-[15px] flex basis-[33%] flex-col items-center "
           key={id}
         >
           <img
             src={img}
             className={`h-[66px] w-[66px] cursor-pointer rounded-full object-cover object-center ${
-              id === 1 && 'border-[1px] border-black'
-            } `}
+              id === active && 'border-[1px] border-black'
+            } box-border`}
+            onClick={() => setActive(id)}
           />
-          <span className="font-muli cursor-pointer text-[13px] font-extrabold ">
+          <span
+            className="font-muli max-width-[34%] cursor-pointer text-[13px] font-extrabold "
+            onClick={() => setActive(id)}
+          >
             {name}
           </span>
         </div>
