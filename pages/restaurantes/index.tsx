aa359e14fd22bc87fc546f92f6e678b138980b13
Krb0 <maurodavidcaceres@gmail.com>
2022-05-18 20:10:00 -0300

    loading added

--- pages/restaurantes/index.tsx
+++ pages/restaurantes/index.tsx
@@ -1,11 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import LeftPanel from '../../components/Restaurantes/LeftPanel'
 import MiddlePanel from '../../components/Restaurantes/MiddlePanel'
 import RightPanel from '../../components/Restaurantes/RightPanel'
 import Layout from '../../Layout'
-
 const Restaurantes: NextPage = () => {
+  const [loading, setLoading] = useState(true)
+  const address = useSelector((state: any) => state.user.address)
+  const router = useRouter()
+  useEffect(() => {
+    if (address === '') {
+      router.push('/')
+    } else {
+      setLoading(false)
+    }
+  }, [address])
+
+  if (loading)
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <img
+          src={
+            'https://live.pystatic.com/webassets/AppscoreWeb/monolith/4.0.48/images/monolith-shopListLoader.cca36f65.png'
+          }
+        />
+      </div>
+    )
   return (
     <Layout>
       <div className="max-w-screen flex min-h-screen bg-[#F9F6F4] pt-[24px] ">
