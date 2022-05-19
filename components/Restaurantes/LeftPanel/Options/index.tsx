e2533aaf9098abeb1c0076cd8a2916889755b585
Krb0 <maurodavidcaceres@gmail.com>
2022-05-19 19:37:25 -0300

    categories added

--- components/Restaurantes/LeftPanel/Options/index.tsx
+++ components/Restaurantes/LeftPanel/Options/index.tsx
@@ -1,8 +1,51 @@
- 
+import Image from 'next/image'
+import moreVertIcon from '../../../../assets/icons/shared/moreInfo.svg'
 const Options = () => {
- 
- 
-return <div>  </div>;
-};
- 
-export default Options;
+  const items = [
+    {
+      title: 'Ordenar',
+      options: ['Mejor puntuados', 'Más cercanos', 'Menor tiempo de entrega'],
+    },
+    {
+      title: 'Filtros',
+      options: ['Delivery por PedidosYa'],
+    },
+    {
+      title: 'Promociones',
+      options: ['Cupón', 'Tarjeta', 'Descuentos'],
+    },
+  ]
+  return (
+    <div className="custom-shadow mt-8 p-6">
+      <List items={items} />
+    </div>
+  )
+}
+
+export default Options
+
+const List = ({ items }: { items: { title: string; options: string[] }[] }) => {
+  return (
+    <>
+      {items.map(({ title, options }) => (
+        <>
+          <div style={{ marginTop: 4, marginBottom: 12 }}>
+            <div className="flex cursor-pointer items-center gap-1">
+              <p className="font-muli text-[16px] font-bold ">{title}</p>
+              <div className="relative h-4 w-4">
+                <Image src={moreVertIcon} layout="fill" />
+              </div>
+            </div>
+            <ul className="list-decoration">
+              {options.map((option) => (
+                <li className="cursor-pointer">
+                  <span>{option}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ))}
+    </>
+  )
+}
--- components/Restaurantes/RightPanel/index.tsx
+++ components/Restaurantes/RightPanel/index.tsx
@@ -1,9 +1,64 @@
+import Image from 'next/image'
+import moreVertIcon from '../../../assets/icons/shared/moreInfo.svg'
+
 const RightPanel = () => {
+  const items = [
+    {
+      title: 'Medios de pago',
+      options: ['Pago online', 'Efectivo'],
+    },
+    {
+      title: 'Categorías',
+      options: [
+        'Arepas',
+        'Bebidas',
+        'Cafetería',
+        'Lomitos',
+        'Comida Árabe',
+        'Comida Armenia',
+        'Comida China',
+        'Comida India',
+        'Comida Japonesa',
+        'Comida Mexicana',
+        'Comida Peruana',
+        'Comida Vegetariana',
+        'Ver más...',
+      ],
+    },
+  ]
   return (
-    <div className="flex flex-[1] flex-col ">
-      <span>right</span>
+    <div className=" flex flex-[1] flex-col ">
+      <div className="custom-shadow mt-8 p-6">
+        <List items={items} />
+      </div>
     </div>
   )
 }
 
 export default RightPanel
+
+const List = ({ items }: { items: { title: string; options: string[] }[] }) => {
+  return (
+    <>
+      {items.map(({ title, options }) => (
+        <>
+          <div style={{ marginTop: 8, marginBottom: 12 }}>
+            <div className="flex cursor-pointer items-center gap-1 ">
+              <p className="font-muli text-[16px] font-bold ">{title}</p>
+              <div className="relative h-4 w-4">
+                <Image src={moreVertIcon} layout="fill" />
+              </div>
+            </div>
+            <ul className="font-lato mt-1 text-[14px] font-[400] text-[#2B1A46]">
+              {options.map((option) => (
+                <li className="my-[0.3rem] cursor-pointer">
+                  <span>{option}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ))}
+    </>
+  )
+}
