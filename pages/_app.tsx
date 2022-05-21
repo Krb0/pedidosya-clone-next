70fae449f521e5df70d4cdd92e3035d9fdb31dd2
Krb0 <maurodavidcaceres@gmail.com>
2022-05-21 00:36:38 -0300

    apollo added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,28 @@ import { SessionProvider } from 'next-auth/react'
 import { Provider } from 'react-redux'
 import { CookiesProvider, useCookies } from 'react-cookie'
 import { useEffect } from 'react'
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+  cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
         <CookiesProvider>
-          <App Component={Component} pageProps={pageProps} />
+          <ApolloProvider client={client}>
+            <App Component={Component} pageProps={pageProps} />
+          </ApolloProvider>
         </CookiesProvider>
       </Provider>
     </SessionProvider>
